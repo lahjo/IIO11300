affbe821ac95713bb5ce820d12c63b7ce5dfa5f9
lahjo <h8306@student.jamk.fi>
2016-01-24 00:11:22 +0200

    WinLotto

---
+++ IIO11300Vktehtavat/Tehtävä2/BLLotto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä2
+{
+    class BLLotto {
+        private int[] suomilotto, vikinglotto, eurojackpotPaa, eurojackpotlisa;
+        private string result, resultLisa;
+        Random rd = new Random();
+
+        public string SuomiLotto() {
+            suomilotto = new int[7];
+
+            for(int i=0 ;i < 7 ;i++) {
+                int randomIndex = rd.Next(1, 39);
+                while (suomilotto.Contains(randomIndex)) {
+                    randomIndex = rd.Next(1, 39);
+                }
+                suomilotto[i] = randomIndex;
+            }
+    
+                result = string.Join(" ", suomilotto);
+            return result;
+        }
+
+        public string VikingLotto() {
+                vikinglotto = new int[6];
+
+                for (int i = 0; i < 6; i++) {
+                    int randomIndex = rd.Next(1, 48);
+                    while (vikinglotto.Contains(randomIndex))
+                    {
+                        randomIndex = rd.Next(1, 48);
+                    }
+                    vikinglotto[i] = randomIndex;
+                }
+
+                result = string.Join(" ", vikinglotto);
+            return result;
+        }
+
+        public string Eurojackpot() {
+                eurojackpotPaa = new int[5];
+                eurojackpotlisa = new int[2];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    int randomIndex = rd.Next(1, 50);
+                    while (eurojackpotPaa.Contains(randomIndex))
+                    {
+                        randomIndex = rd.Next(1, 50);
+                    }
+                    eurojackpotPaa[i] = randomIndex;
+                }
+
+                string result = string.Join(" ", eurojackpotPaa);
+
+                for (int i = 0; i < 2; i++)
+                {
+                    int randomIndex = rd.Next(1, 8);
+                    while (eurojackpotlisa.Contains(randomIndex))
+                    {
+                        randomIndex = rd.Next(1, 8);
+                    }
+                    eurojackpotlisa[i] = randomIndex;
+                }
+
+                resultLisa = string.Join(" ", eurojackpotlisa);
+                return result + " | " + resultLisa;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtävä2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtävä2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            component();
+                
+        }
+
+        private void component() {
+            comboBox.Items.Add("SuomiLotto");
+            comboBox.Items.Add("VikingLotto");
+            comboBox.Items.Add("Eurojackpot");
+
+            comboBox.SelectedItem = "SuomiLotto";
+        }
+
+        private void draw_Click(object sender, RoutedEventArgs e) {
+            try
+            {
+                int rounds = int.Parse(roundsNum.Text);
+
+                BLLotto lotto = new BLLotto();
+
+                if (comboBox.SelectedItem == "SuomiLotto") {
+                    for (int roundsLimit = 0; roundsLimit < rounds; roundsLimit++) {
+                        listBox.Items.Add(lotto.SuomiLotto());
+                    }
+                } else if(comboBox.SelectedItem == "VikingLotto") {
+                    for (int roundsLimit = 0; roundsLimit < rounds; roundsLimit++)
+                    {
+                        listBox.Items.Add(lotto.VikingLotto());
+                    }
+                } else if (comboBox.SelectedItem == "Eurojackpot") {
+                    for (int roundsLimit = 0; roundsLimit < rounds; roundsLimit++)
+                    {
+                        listBox.Items.Add(lotto.Eurojackpot());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+    }
+}
