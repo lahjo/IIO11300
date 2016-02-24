3bdd1f71c4a5b7e1ab68b9c83ab6de147e9c76dd
lahjo <H8306@studnet.jamk.fi>
2016-02-24 11:16:34 +0200

    Tuntiharjoitus 6

---
+++ IIO11300Vktehtavat/Harjoitus6-Binding/Hockey.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus6_Binding
+{
+    public class HockeyTeam
+    {
+        #region PROPERTIES
+        // Huom public field ei keplaa WPF DataBinding:ssa, pitää olla Property
+        public string Name { get; set; }
+        public string City { get; set; }
+        #endregion
+
+        #region CONSTRUCTOR
+        public HockeyTeam() {
+            Name = "";
+            City = "unknown";
+        }
+
+        public HockeyTeam(string name, string city) {
+            this.Name = name;
+            this.City = city;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return Name + "@" + City; 
+        }
+    }
+
+    public class HockeyLeague {
+        // Pertustetaan SM-Liiga, sisältää x kpl joukkueita
+        List<HockeyTeam> teams = new List<HockeyTeam>();
+
+        public HockeyLeague()
+        {
+            teams.Add(new HockeyTeam("HIFK", "HELSINKI"));
+            teams.Add(new HockeyTeam("JYP", "Jyväskylä"));
+            teams.Add(new HockeyTeam("Kalpa", "Kuopio"));
+            teams.Add(new HockeyTeam("Sport", "Vaasa"));
+        }
+
+        // Metodi joka palauttaa Liigan joukkueet
+        public List<HockeyTeam> GetTeams() {
+            return teams;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Harjoitus6-Binding/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus6_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HockeyLeague smliiga;
+        List<HockeyTeam> joukkueet;
+        int clicked = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillMyComboBox();
+            smliiga = new HockeyLeague();
+            joukkueet = smliiga.GetTeams();
+        }
+
+        private void btnSetUser_Click(object sender, RoutedEventArgs e)
+        {
+            // Luetaan App.Configista UserName-asetus
+            txtUsername.Text = "Terve käyttäjä: " + Harjoitus6_Binding.Properties.Settings.Default.UserName;
+        }
+
+        private void FillMyComboBox() {
+            cbCourses2.Items.Add("IIO12110 Ohjelmistotuotanto");
+            cbCourses2.Items.Add("Ruotsi");
+        }
+
+        private void btnForword_Click(object sender, RoutedEventArgs e)
+        {
+            clicked++;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+
+        private void btnBackword_Click(object sender, RoutedEventArgs e)
+        {
+            clicked--;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+
+        private void btnBind_Click_1(object sender, RoutedEventArgs e)
+        {
+            myGrid.DataContext = joukkueet;
+        }
+    }
+}
